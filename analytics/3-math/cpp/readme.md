5b65ce682349a547189cdc34ff898c3ce03ff827
Evgenii Novikov <enovikov11@yandex.ru>
2026-08-25 20:56:30 +0200

    Delete analytics/3-math directory

--- analytics/3-math/cpp/readme.md
+++
@@ -1,2 +0,0 @@
-g++ file.cpp -o file.elf -std=c++11
-// g++ -I /usr/local/Cellar/opencv/4.2.0_1/include/opencv4 cv.cpp -o cv.elf -std=c++14
--- analytics/3-math/haskell/fibonacci/fib.hs
+++
@@ -1,7 +0,0 @@
-fibonacci :: Int -> Integer
-fibonacci n = fst $ foldl (\(a, b) _ -> (b, a + b)) (0, 1) [1..n]
-
-main :: IO()
-main = do
-    input <- getLine
-    putStrLn.show.fibonacci.read $ input
